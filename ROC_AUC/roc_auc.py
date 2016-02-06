ca1f68c4caeb9d8ff00bac0707b2df1a7f33202c
ervachon <eric.vachon.pro@orange.fr>
2016-02-06 18:38:59 +0100

    AUC

--- ROC_AUC/roc_auc.py
+++ ROC_AUC/roc_auc.py
@@ -15,38 +15,35 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-myDF = pd.read_csv(os.path.join(os.getcwd(), "data_test.csv"),sep=";",low_memory=False)
-myDF["y_true"] = myDF["y_true"].astype('float64')
-myDF["y_proba"] = myDF["y_proba"].astype('float64')
+myDF1 = pd.read_csv(os.path.join(os.getcwd(), "data_test_1.csv"),sep=";",low_memory=False)
+myDF1["y_true"] = myDF1["y_true"].astype('float64')
+myDF1["y_proba"] = myDF1["y_proba"].astype('float64')
 
-NbN = len(myDF[(myDF.y_true == 0)])
-NbP = len(myDF[(myDF.y_true == 1)])
+NbN = len(myDF1[(myDF1.y_true == 0)])
+NbP = len(myDF1[(myDF1.y_true == 1)])
 
 plt.axis([-0.1, 1.1, -0.1, 1.1])
 plt.plot([1,0,0,1,],[0,0,1,1,] , 'g-')   
 
-lP=[[1.],[0.],[0.]]
+lP=pd.DataFrame({"x":[1.], "y":[0.], "aire":[0.]})
 
 for seuil in np.arange(0,1.05,0.05):
-   sub = myDF[(myDF.y_proba >= seuil)]
+   sub = myDF1[(myDF1.y_proba >= seuil)]
    TVP = 1.*len(sub[(sub.y_true == 1)])/NbP
    FVP = 1.*len(sub[(sub.y_true == 0)])/NbN
    print("> seuil : " + str(seuil) + ", (" + str(FVP) + "," + str(TVP) + ")")
    
    x1 = FVP
-   x2 = lP[0][len(lP[0])-1]
+   x2 = lP.x[len(lP)-1]
    y1 = TVP
-   y2 = lP[1][len(lP[0])-1]
+   y2 = lP.y[len(lP)-1]
    aire = np.abs(1.*np.min([y1,y2])*(x1-x2)) + np.abs((y1-y2)*(x1-x2)/2.)
-   lP[0] = lP[0] + [FVP]
-   lP[1] = lP[1] + [TVP]
-   lP[2] = lP[2] + aire
+  
+   lP.loc[len(lP)]={"x":FVP, "y":TVP, "aire":aire}
 
-lP[0] = lP[0] + [0.]
-lP[1] = lP[1] + [0.]
 
-print( "> AUC = " + str(np.sum(lP[2])))
+print( "> AUC = " + str(np.sum(lP.aire.values)))
 
-plt.plot(lP[0], lP[1], linestyle='-')   
+plt.plot(lP.x.values, lP.y.values, linestyle='-')   
 plt.plot([0,1],[0,1] , 'r-')   
 plt.show()
